589d871e040c3cf4f7e66b2419579cb79ed44e1f
aranesy <aranesy@gmail.com>
2016-09-22 18:35:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var app = express();
-
-// Routes
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-// Listen
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Listening on localhost:'+ port);
+var http = require("http");
+    
+http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello\r\n");
+    res.write("World\r\n");
+    res.end();
+}).listen(8080);
